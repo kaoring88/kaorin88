96dbe39bb451f9e3e0e5655dd49fdd1b60f6b892
kaoring88 <kabochi8spiral@gmail.com>
2024-02-07 03:38:26 +0900

    a

--- import openpyxl as excel.py
+++
@@ -1,6 +0,0 @@
-import openpyxl as excel
-
-wbname = "card.xlsx"
-wb = excel.Workbook()
-wb.save("card.xlsx")
-
--- worksheetnamechange.py
+++
@@ -1,7 +0,0 @@
-import openpyxl as excel
-
-wb = excel.load_workbook('C:/Users/かおりん/Desktop/poker/card.xlsx')
-ws = wb.worksheets[0]
-
-ws.title = 'card'
-wb.save("card.xlsx")
